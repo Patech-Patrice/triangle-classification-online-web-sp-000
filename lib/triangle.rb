512e4c445961e295d2f10fc97501f488ea7689ee
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-06 02:09:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,28 +2,36 @@
 
 
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
- 
-  def initialize(:equilateral, :isosceles, :scalene)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
-    end
- 
-  def kind(triangle)
-    self.partner = triangle
-    if triangle.class != Triangle
-      
-    
-      rescue TriangleError => error
-          puts error.message
-      
-    else
-      triangle.partner = self
-    end
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @b = b
+    @c = c
   end
- 
-  class TriangleError < StandardError
+
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if a == b && b == c && a == c
+      :equilateral
+    elsif a != b && b != c && c != a
+      :scalene 
+    else 
+      :isosceles
+    end 
   end
   
+  def kosher_triangle 
+    a.positive? && b.positive? && c.positive?
+  end 
+  
+  def kosher_triangle2 
+    a + b > c && a + c > b && b + c > a
+  end 
+end
+
+class TriangleError < StandardError
 end
