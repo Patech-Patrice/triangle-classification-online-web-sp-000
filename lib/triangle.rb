a62c4e46891739e823e7ca49bd4c8875819b01bf
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-06 02:14:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,39 +31,5 @@ def kind()
 end
 
 
-
-
-
-
-
-
-
-
-
-  def kind
-    if kosher_triangle == true && kosher_triangle2 == true 
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
-    if a == b && b == c && a == c
-      :equilateral
-    elsif a != b && b != c && c != a
-      :scalene 
-    else 
-      :isosceles
-    end 
-  end
-  
-  def kosher_triangle 
-    a.positive? && b.positive? && c.positive?
-  end 
-  
-  def kosher_triangle2 
-    a + b > c && a + c > b && b + c > a
-  end 
-end
-
 class TriangleError < StandardError
 end
